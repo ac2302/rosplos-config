91473dd78ea18aabd7e40d9998fef80d3fd7784e
Aryan Chavan <aryan@aryansdomain.com>
2023-10-26 01:26:37 +0530

    make startup script pull instead of making it do weird stuff

--- startup.sh
+++ startup.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 
-config_repo="https://github.com/ac2302/rosplos-config"
-
 # cloning repo
-rm -rf ~/.rosplos-config
-git clone $config_repo ~/.rosplos-config
+# git clone $config_repo ~/.rosplos-config
+git pull
 
 # running the refresh script
 bash ~/.rosplos-config/refresh.sh
